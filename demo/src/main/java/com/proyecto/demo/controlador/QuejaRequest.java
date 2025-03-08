fa77236255d12814072156df4cdbdfe1f40436aa
SDM30 <simondiaz@javeriana.edu.co>
2025-03-08 17:38:52 -0500

    Actualizacion de DataBaseInit

---
+++ demo/src/main/java/com/proyecto/demo/controlador/QuejaRequest.java
@@ -0,0 +1,62 @@
+package com.proyecto.demo.controlador;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import java.util.Date;
+
+public class QuejaRequest {
+    @JsonFormat(pattern = "yyyy-MM-dd")
+    private Date fecha;
+    private String tipo;
+    private String descripcion;
+    private Long idServicio;
+    private Long idEmpresa;
+    private Long idUsuario;
+
+    // Getters y Setters
+    public Date getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(Date fecha) {
+        this.fecha = fecha;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public Long getIdServicio() {
+        return idServicio;
+    }
+
+    public void setIdServicio(Long idServicio) {
+        this.idServicio = idServicio;
+    }
+
+    public Long getIdUsuario() {
+        return idUsuario;
+    }
+
+    public void setIdUsuario(Long idUsuario) {
+        this.idUsuario = idUsuario;
+    }
+
+    public Long getIdEmpresa() {
+        return idEmpresa;
+    }
+
+    public void setIdEmpresa(Long idEmpresa) {
+        this.idEmpresa = idEmpresa;
+    }
+}
