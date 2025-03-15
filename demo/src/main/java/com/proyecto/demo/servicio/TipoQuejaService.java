0fb2d4bb19bdc49c5d0b1e1d3c71cc568282e1c2
SDM30 <simondiaz@javeriana.edu.co>
2025-03-14 23:19:58 -0500

    PENDIENTE: Que el usuario pueda ver las quejas con el formato de vista de quejas

---
+++ demo/src/main/java/com/proyecto/demo/servicio/TipoQuejaService.java
@@ -0,0 +1,11 @@
+package com.proyecto.demo.servicio;
+
+import java.util.List;
+
+import com.proyecto.demo.entidad.TipoQueja;
+
+public interface TipoQuejaService {
+    //Buscar por descripcion del tipo de queja
+    public TipoQueja buscarTipoQueja(String descripcion);
+    public List<TipoQueja> obtenerTodosLosTiposQueja();
+}
---
+++ demo/src/main/java/com/proyecto/demo/servicio/TipoQuejaServiceImpl.java
@@ -0,0 +1,27 @@
+package com.proyecto.demo.servicio;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.proyecto.demo.entidad.TipoQueja;
+import com.proyecto.demo.repositorio.TipoQuejaRepository;
+
+@Service
+public class TipoQuejaServiceImpl implements TipoQuejaService {
+
+    @Autowired
+    private TipoQuejaRepository tipoQuejaRepository;
+    //Buscar por descripcion del tipo de queja
+    @Override
+    public TipoQueja buscarTipoQueja(String descripcion) {
+        return tipoQuejaRepository.findByDescripcion(descripcion);
+    }
+    
+    @Override
+    public List<TipoQueja> obtenerTodosLosTiposQueja() {
+        return tipoQuejaRepository.findAll();
+    }
+
+}
